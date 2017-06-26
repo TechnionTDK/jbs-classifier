c3d610705bf13a771644d3f1f6fdb5088257b58a
NetanelFelcher <netanel_felcher@hotmail.com>
2017-06-26 16:01:23 +0300

    add runner class

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,40 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+
 /**
  * Created by netanel on 26/06/2017.
  */
 public class Runner {
+    JsonList list;
 
+    public Runner(){
+        this.list=new JsonList();
+    }
+
+    public void run(){
+        for(String url:new Queries().getAllWikipediaPages()){
+            ArrayList<String> sources=new ArrayList<String>();//=call Topic with url ang get all sources strings
+            JsonTuple jt=new JsonTuple();
+            jt.setUri(url);
+            for(String source:sources){
+                ArrayList<String> uris=new UriConverter(source).getUris();
+                jt.setMentions(uris);
+            }
+            this.list.addJsonTuple(jt);
+
+        }
+        FileWriter writer;
+        try {writer = new FileWriter("Output.json") ;
+            Gson gson=new GsonBuilder().setPrettyPrinting().create();
+            String tupleJson = gson.toJson(this.list);
+            gson.toJson(tupleJson);
+            writer.write(tupleJson);
+            writer.close();
+        }
+        catch(Exception e){}
+
+    }
 }
