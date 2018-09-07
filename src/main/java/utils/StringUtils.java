f825778d4e51372fab46a5da72af6a554db8e552
avnerec <avner19@walla.com>
2018-09-07 21:15:59 +0300

    utils package

---
+++ src/main/java/utils/StringUtils.java
@@ -0,0 +1,64 @@
+package utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+/*
+* util static class adding some actions to do on strings.
+*/
+
+public class StringUtils {
+
+    /* look for sub strings of elementText matching regex */
+    public static Matcher findRegInString(String elementText, String regex) {
+        Pattern p = Pattern.compile(regex, Pattern.DOTALL);
+        Matcher m = p.matcher(elementText);
+        return m;
+    }
+
+    /* remove all strings in badWords from reference string */
+    public static String cleanStrings(String text, List<String> badStrings, String replacement) {
+        for (String badString : badStrings) {
+            //reference = reference.replaceAll("(?i)\\b[^\\w -]*" + badWord + "[^\\w -]*\\b", "");
+            text = text.replaceAll(badString, replacement);
+        }
+        return text;
+    }
+
+    public static String cleanStringsNoSpace(String text, List<String> badStrings){
+        return  cleanStrings(text,badStrings,"");
+    }
+
+    public static String cleanStringsSpace(String text, List<String> badStrings){
+        return  cleanStrings(text,badStrings," ");
+    }
+
+    public static String cleanExtraSpaces(String text){
+        return  cleanStringsSpace(text, Arrays.asList("(?i)[\\s]+"));
+    }
+
+    /* remove all strings in badWords from reference string */
+    public static String cleanWords(String reference, List<String> badWords) {
+        for (String badWord : badWords) {
+            reference = reference.replaceAll("(?i)[\\s]+" + badWord + "(?i)[\\s]+", " ");
+        }
+        return reference;
+    }
+
+
+    public static List<String> insidePatern(String pref, String suff, String data) {
+        List<String> foundStrings = new LinkedList<String>();
+        Pattern pattern = Pattern.compile(pref + "(.*?)" + suff);
+        Matcher matcher = pattern.matcher(data);
+        while (matcher.find())
+        {
+            foundStrings.add(matcher.group(1));
+        }
+        return  foundStrings;
+    }
+}
